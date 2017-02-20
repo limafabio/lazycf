72cda56d154cdf20f5defb17c2b5b64ac9a3d0ea
fabio lima <fabiolima39@gmail.com>
2017-02-20 16:11:00 -0300

    class contest and unit test

--- tests/Test_Classes.py
+++ tests/Test_Classes.py
@@ -5,12 +5,18 @@ import sys
 import unittest
 sys.path.append(os.path.abspath('..'))
 from sample.Entity import Entity
+from sample.Contest import Contest
+
 
 class TestClasses(unittest.TestCase):
 
     def test_init(self):
         entity_test = Entity('1', 'foo')
         self.assertEqual(str(entity_test.__class__), "sample.Entity.Entity")
+
+    def test_contest(self):
+        contest_test = Contest('2', 'bar')
+        self.assertEqual(str(contest_test.__class__), "sample.Contest.Contest")
 
 if __name__ == "__main__":
     unittest.main()
