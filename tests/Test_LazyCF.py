9ce219d94f2fe85f852ed6baa546add3b138ad22
fabio lima <fabiolima39@gmail.com>
2017-02-22 18:08:09 -0300

    class lazyCf with unit tests

---
+++ tests/Test_LazyCF.py
@@ -0,0 +1,47 @@
+#!/usr/bin/py
+
+import os
+import sys
+import shutil
+import unittest
+sys.path.append(os.path.abspath('..'))
+from sample.LazyCF import LazyCF
+from sample.CodeForces import CodeForces
+
+
+class TestLazyCF(unittest.TestCase):
+
+    def test__init__(self):
+        lazy_test = LazyCF()
+        self.assertEqual(str(lazy_test.__class__), "sample.LazyCF.LazyCF")
+
+    def test_get_contest(self):
+        cf = LazyCF()
+        cf.get_contest(768)
+        self.assertEqual(cf.contest.id, 768)
+
+    def test_create_folder(self):
+        cf = CodeForces()
+        contest_test = cf.get_contest(768)
+        lazy = LazyCF()
+        lazy.create_folder(contest_test)
+        path = os.path.abspath('.')
+        path += "/" + contest_test.problem_list[0].index
+        self.assertTrue(os.path.isdir(path))
+        shutil.rmtree(path)
+
+    '''
+    def test_create_file(self):
+        cf = CodeForces()
+        contest_test = cf.get_contest(768)
+        lazy = LazyCF()
+        lazy.create_folder(contest_test)
+        path = os.path.abspath('.')
+        path += "/" + contest_test.problem_list[0].index
+        print "EH o bicho " + str(len(contest_test.problem_list[0].test))
+        lazy.create_file(contest_test.problem_list[0].test[0],
+                         contest_test.problem_list[0], path)
+
+    '''
+if __name__ == "__main__":
+    unittest.main()
